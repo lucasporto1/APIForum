0f1829db451141c0734e7eafb3b124b22712338a
Lucas Porto <cslukas155@gmail.com>
2022-03-19 21:33:53 -0300

    criando pageable

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,13 +2,16 @@ package br.com.alura.forum.controller;
 
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,7 +42,7 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
-	@GetMapping
+	/*@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		System.out.println(nomeCurso);
 		if (nomeCurso == null) {
@@ -46,6 +50,35 @@ public class TopicosController {
 			return TopicoDto.converter(topicos);
 		} else {
 			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}*/
+	
+	//refazer a lista de forma pageable
+	/*@GetMapping
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
+		
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
+		System.out.println(nomeCurso);
+		if (nomeCurso == null) {
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+			return TopicoDto.converter(topicos);
+		} else {
+			Page<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso, paginacao);
+			return TopicoDto.converter(topicos);
+		}
+	}*/
+	
+	//simplificando o pageable
+	@GetMapping
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,@PageableDefault(sort="id", direction = Direction.DESC, page=0, size=10) Pageable paginacao) {
+		
+		System.out.println(nomeCurso);
+		if (nomeCurso == null) {
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+			return TopicoDto.converter(topicos);
+		} else {
+			Page<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso, paginacao);
 			return TopicoDto.converter(topicos);
 		}
 	}
